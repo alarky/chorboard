01c950db8972da582b5270e27bb6217c01a91367
alarky <alarky@gmail.com>
2021-04-14 00:59:31 +0900

    init main vue

--- src/chorboard/src/views/Main.vue
+++ src/chorboard/src/views/Main.vue
@@ -1,14 +1,11 @@
 <template>
-  <router-view/>
+  Main
 </template>
-
-<style lang="scss">
-</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App',
+  name: 'Main',
 });
 </script>
