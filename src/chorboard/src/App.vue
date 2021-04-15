db742540ff31bb281ab188c556b1baa4183a1a75
alarky <alarky@gmail.com>
2021-04-16 01:13:37 +0900

    init tailwind css

--- src/chorboard/src/App.vue
+++ src/chorboard/src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <router-view/>
+  <div class="h-screen flex justify-between flex-col">
+    <router-link to="/">
+      <h1 class="h-24 p-3 flex items-center bg-green-500 text-white text-2xl font-bold"> TODO APP composition API </h1>
+    </router-link>
+  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
+/*
 html, body {
   height: 100%;
   margin: 0;
@@ -12,6 +18,8 @@ html, body {
   background-color: gray;
   color: white;
 }
+
+ */
 </style>
 
 <script lang="ts">
--- src/chorboard/src/main.ts
+++ src/chorboard/src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import './registerServiceWorker'
+import './registerServiceWorker'
 import router from './router'
 import { store, key } from './store'
+import './assets/scss/tailwind.scss'
 
 const app = createApp(App);
 
