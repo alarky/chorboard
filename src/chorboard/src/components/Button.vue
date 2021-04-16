dea4c6392633282e6aa697e114c7f135deef4e72
alarky <alarky@gmail.com>
2021-04-16 23:44:06 +0900

    init button component

---
+++ src/chorboard/src/components/Button.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="button-square" :class="{on: state.pushed, off: !state.pushed}" @click="toggle">
+    {{ label }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.button-square {
+  @apply relative;
+  @apply rounded;
+  @apply select-none;
+}
+
+.on {
+  box-shadow: inset  2px  2px 5px #aaaaaa,
+              inset -2px -2px 5px #ffffff;
+}
+
+.off {
+  box-shadow: 2px  2px 5px #aaaaaa,
+             -2px -2px 5px #ffffff;
+}
+
+</style>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+
+interface State {
+  pushed: boolean;
+}
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      default: () => '',
+    },
+  },
+  setup() {
+    const state = reactive<State>({
+      pushed: false,
+    });
+
+    const toggle = () => {
+      state.pushed = !state.pushed;
+    }
+
+    return {
+      state,
+      toggle,
+    }
+  }
+});
+</script>
--- src/chorboard/src/views/Main.vue
+++ src/chorboard/src/views/Main.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-    <label for="full-velocity">FullVelocity</label>
-    <input type="checkbox" id="full-velocity" v-model="fullLevel">
-  </div>
-  <div>
-    <label for="octave">Octave</label>
-    <input type="number" id="octave" v-model="octave">
+    <div class="font-header italic mb-1">settings</div>
+    <div class="flex flex-row">
+      <Button class="mx-1 my-1 px-4 py-2 w-40" :label="'FullVelocity'"></Button>
+      <Button class="mx-1 my-1 px-4 py-2 w-40"></Button>
+      <Button class="mx-1 my-1 px-4 py-2 w-40"></Button>
+    </div>
+
+    <!--
+    <div>
+      <label for="full-velocity">FullVelocity</label>
+      <input type="checkbox" id="full-velocity" v-model="fullLevel">
+    </div>
+    <div>
+      <label for="octave">Octave</label>
+      <input type="number" id="octave" v-model="octave">
+    </div>
+    -->
   </div>
 </template>
 
 <script lang="ts">
 import {computed, defineComponent} from 'vue';
 import { useStore } from '@/store';
+import Button from "@/components/Button.vue";
 
 export default defineComponent({
   name: 'Main',
+  components: {Button},
   setup() {
     const store = useStore();
 
