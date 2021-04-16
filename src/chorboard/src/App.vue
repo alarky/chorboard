356809cc6ea0d9c7de7632b5b68d8fcf85e42293
alarky <alarky@gmail.com>
2021-04-16 22:44:19 +0900

    init theme and header

--- src/chorboard/src/App.vue
+++ src/chorboard/src/App.vue
@@ -1,33 +1,29 @@
 <template>
-  <div class="h-screen flex justify-between flex-col">
-    <router-link to="/">
-      <h1 class="font-header text-4xl italic">ChorBoard</h1>
-      <p>This app is choard assist keyboard.</p>
-    </router-link>
-    <router-view />
+  <div class="app">
+    <div class="container mx-auto py-4">
+      <Header></Header>
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-/*
-html, body {
-  height: 100%;
-  margin: 0;
+.app {
+  @apply min-h-screen;
+  @apply bg-gray-300;
+  @apply text-gray-600;
 }
-#app {
-  min-height: 100%;
-  background-color: gray;
-  color: white;
-}
-
- */
 </style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Header,
+  },
   setup() {
     return {
     }
---
+++ src/chorboard/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="flex items-baseline flex-row mb-4">
+    <h1 class="font-header text-4xl italic">ChorBoard</h1>
+    <p class="mx-4">choard/scale assist keyboard.</p>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+  setup() {
+    return {
+    }
+  }
+});
+</script>
