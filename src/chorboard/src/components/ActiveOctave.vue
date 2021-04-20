eea5596ba54f514d8abe8c1fd523de83d89ba4e3
alarky <alarky@gmail.com>
2021-04-20 09:52:37 +0900

    add active octave component

---
+++ src/chorboard/src/components/ActiveOctave.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="active-octave">
+    <div class="label-container">
+      <label for="active-octave-input">OCTAVE</label>
+    </div>
+    <div>
+      <input type="number" id="active-octave-input" v-model="activeOctave" min="0" max="9">
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.active-octave {
+  @apply flex;
+  @apply flex-col;
+}
+
+.label-container {
+  @apply flex;
+  @apply justify-center;
+  @apply pr-1;
+}
+
+input {
+  @apply m-0;
+}
+
+</style>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import {useStore} from "@/store";
+
+export default defineComponent({
+  name: 'ActiveOctave',
+  setup() {
+    const store = useStore();
+
+    const activeOctave = computed({
+      get: () => store.state.activeOctave,
+      set: (v) => store.commit('activeOctave', {v: Number(v)}),
+    });
+
+    return {
+      activeOctave,
+    }
+  }
+});
+</script>
--- src/chorboard/src/views/Main.vue
+++ src/chorboard/src/views/Main.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <div class="flex flex-row mb-4">
-      <h1 class="font-header text-4xl italic">ChorBoard</h1>
+    <div class="header">
+      <h1>ChorBoard</h1>
     </div>
 
-    <div class="mb-4">
+    <div class="controls">
+      <ActiveOctave></ActiveOctave>
+
       <input type="text" class="input" />
       <button class="button">button</button>
       <select class="select">
@@ -20,14 +22,37 @@
   </div>
 </template>
 
+<style scoped lang="scss">
+.header {
+  @apply flex;
+  @apply flex-row;
+  @apply mb-4;
+
+  h1 {
+    @apply text-4xl;
+    @apply italic;
+  }
+}
+
+.controls {
+  @apply flex;
+  @apply flex-row;
+  @apply mb-4;
+}
+</style>
+
 <script lang="ts">
 import {computed, defineComponent} from 'vue';
 import { useStore } from '@/store';
 import KeyBoard from "@/components/KeyBoard.vue";
+import ActiveOctave from "@/components/ActiveOctave.vue";
 
 export default defineComponent({
   name: 'Main',
-  components: {KeyBoard},
+  components: {
+    KeyBoard,
+    ActiveOctave,
+  },
   setup() {
     const store = useStore();
 
